40d69a291af1bb17e2c4786de944588545a38eb6
reggiechan <reggy.chan@gmail.com>
2016-10-06 16:33:38 -0400

    Add files via upload

---
+++ StartGameScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour {
+
+	//when start button is pressed.
+	public void OnGameStart(string s){
+		Debug.Log ("you pressed start");
+
+		//Add load level for new scene here.
+
+		SceneManager.LoadScene (s);
+	}
+}
