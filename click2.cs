40169b02073977c03edf3ffcd7982ea51def409a
erickher <erickher@buffalo.edu>
2016-10-07 01:35:59 -0400

    Add files via upload

---
+++ click2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click2 : MonoBehaviour
+{
+    public Main2 main;
+    public void OnMouseDown()
+    {
+        Debug.Log("clcked0");
+        main.testCorrect(0);
+
+    }
+}
+
+
