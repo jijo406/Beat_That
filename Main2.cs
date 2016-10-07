d50a05d510f61e177ab16f62fdc591dd4365604a
erickher <erickher@buffalo.edu>
2016-10-07 17:31:38 -0400

    Add files via upload

--- Main2.cs
+++ Main2.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine.SceneManagement;
+
+
 public class Main2 : MonoBehaviour
 {
     private static int counting = 1;
@@ -10,7 +12,9 @@ public class Main2 : MonoBehaviour
     private GameObject Yellow;
     private GameObject Green;
     private GameObject Blue;
-    public Editor edit;
+    private GameObject Purple;
+    private GameObject Orange;
+    
     /*private Material RedMaterial;
     private Material BlueMaterial;
     private Material GreenMaterial;
@@ -21,7 +25,7 @@ public class Main2 : MonoBehaviour
     int onInList = 0;
     int r;
     private string NewBeatThatProto;
-    static int count = 0;
+
 
     void Start()
     {
@@ -29,11 +33,26 @@ public class Main2 : MonoBehaviour
         Yellow = GameObject.Find("/Cubes/Yellow");
         Green = GameObject.Find("/Cubes/Green");
         Blue = GameObject.Find("/Cubes/Blue");
+        Purple = GameObject.Find("/Cubes/Purple");
+        Orange = GameObject.Find("/Cubes/Orange");
         //for(count; count < counting; count++)
         //{
+        if(counting >= 9)
+        {
+            r = Random.Range(0, 6);
+            pattern.Add(r);
+        }
+
+        else if (counting > 4 && counting < 9)
+        {
             r = Random.Range(0, 4);
             pattern.Add(r);
-
+        }
+        else if (counting <= 4)
+        {
+            r = Random.Range(0, 2)*2;
+            pattern.Add(r);
+        }
         //}
         
         //Debug.Log(pattern[0]);
@@ -63,7 +82,7 @@ public class Main2 : MonoBehaviour
 
     IEnumerator playBack()
     {
-        Debug.Log(pattern.Count + " start ");
+        Debug.Log(pattern.Count + " start " +"jlkj");
         play_Back = true;
         if (pattern != null)
         {
@@ -72,41 +91,47 @@ public class Main2 : MonoBehaviour
 
                 if (pattern[x] == 0)
                 {
+                    //Red.transform.Rotate(Vector3.up, Time.deltaTime * 50, Space.World);
                     //pattern.Remove(x);
                     Red.GetComponent<Renderer>().material.color = Color.white;
                     yield return new WaitForSeconds(0.6f);
                     Red.transform.GetComponent<Renderer>().material.color = Color.red;
-                    yield return new WaitForSeconds(0.6f);
+                    yield return new WaitForSeconds(0.5f);
                     continue;
                 }
                 else if (pattern[x] == 1)
                 {
+                    //Green.transform.Rotate(Vector3.up, Time.deltaTime * 50, Space.World);
                     //pattern.Remove(x);
                     Green.GetComponent<Renderer>().material.color = Color.white;
                     yield return new WaitForSeconds(0.6f);
                     Green.GetComponent<Renderer>().material.color = Color.green;
-                    yield return new WaitForSeconds(0.6f);
+                    yield return new WaitForSeconds(0.5f);
                     continue;
                 }
                 else if (pattern[x] == 2)
                 {
+                    //Blue.transform.Rotate(Vector3.up, Time.deltaTime * 50, Space.World);
                     //pattern.Remove(x);
                     Blue.GetComponent<Renderer>().material.color = Color.white;
                     yield return new WaitForSeconds(0.6f);
                     Blue.GetComponent<Renderer>().material.color = Color.blue;
-                    yield return new WaitForSeconds(0.6f);
+                    yield return new WaitForSeconds(0.5f);
                     continue;
                 }
                 else if (pattern[x] == 3)
                 {
+                    //Yellow.transform.Rotate(Vector3.up, Time.deltaTime * 50, Space.World);
                     //pattern.Remove(x);
                     Yellow.GetComponent<Renderer>().material.color = Color.white;
                     yield return new WaitForSeconds(0.6f);
                     Yellow.GetComponent<Renderer>().material.color = Color.yellow;
-                    yield return new WaitForSeconds(0.6f);
+                    yield return new WaitForSeconds(0.5f);
                     continue;
+
                 }
-                yield return new WaitForSeconds(0.7f);
+            
+                //yield return new WaitForSeconds(0.7f);
             }
         }
         play_Back = false;
@@ -115,23 +140,44 @@ public class Main2 : MonoBehaviour
 
     public void testCorrect(int x)
     {
-        if (play_Back) { return; }
-        if (pattern[onInList]==x)
+        Debug.Log(pattern.Count + "        dfsdfsdfdsfdsfsfsdfdsfdsfdsfdfds");
+        if (play_Back == true) {
+
+            counting = 1;
+            pattern = new List<int>();
+            SceneManager.LoadScene("BeatThatProtoLevel1");
+        }
+        if(pattern[onInList] != x)
+        {
+            
+            counting = 1;
+            pattern = new List<int>();
+            SceneManager.LoadScene("BeatThatProtoLevel1");
+        }
+        else if (pattern[onInList]==x)
         {
             Debug.Log(pattern.Count + "            int " + x);
             onInList++;
             Debug.Log(pattern.Count +"    vs    oninlist: "    + onInList);
-           if(onInList == pattern.Count) {
+           if(onInList >= pattern.Count) {
                 counting++;
-                SceneManager.LoadScene("NewBeatThatProto");
+                if(counting > 8)
+                {
+                    SceneManager.LoadScene("BeatThatProtoLevel2");
+                }
+
+                if(counting > 4 && counting < 9)
+                {
+                    SceneManager.LoadScene("NewBeatThatProto");
+                }
+                if( counting <= 4)
+                {
+                    SceneManager.LoadScene("BeatThatProtoLevel1");
+                }
+                //SceneManager.LoadScene("NewBeatThatProto");
            }
         }
-        else
-        {
-            counting = 1;
-            pattern = new List<int>();
-            SceneManager.LoadScene("NewBeatThatProto");
-        }
+       
 
     }
 }
---
+++ click 5.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click5 : MonoBehaviour {
+
+    public Main2 main;
+    public void OnMouseDown()
+    {
+        Debug.Log("clcked4");
+        main.testCorrect(4);
+    }
+
+    void OnSelect()
+    {
+
+        ;
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            //  var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            // rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+        OnMouseDown();
+    }
+}
