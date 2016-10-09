142cb24327f75b08fdcae37505a1444037d3ecca
satnamsingh1 <satnamsi@buffalo.edu>
2016-10-09 16:51:01 -0400

    Add files via upload

---
+++ click3.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click3 : MonoBehaviour
+{
+    public Main main;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit,100))
+            {
+            
+
+                Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object              
+                Debug.Log("clcked2");
+                main.testCorrect(2); 
+
+                /*         void OnSelect()
+             {
+
+
+                 // If the sphere has no Rigidbody component, add one to enable physics.
+                 if (!this.GetComponent<Rigidbody>())
+                 {
+                    // var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+                   //  rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+                 }
+                 OnMouseDown();*/
+            }
+        }
+    }/*
+    void OnSelect()
+    {
+
+
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+        blueclick();
+        // OnMouseDown();
+    }*/
+}
