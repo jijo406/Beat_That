5ee98ead106f77eff1c995e30adf850f2b99288c
satnamsingh1 <satnamsi@buffalo.edu>
2016-10-28 19:16:27 -0400

    Add files via upload

---
+++ flashanimation.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class flashanimation {
+
+   
+    public IEnumerator playBack(List<int> pattern, GameObject Red, GameObject Blue)
+    {
+        Debug.Log("in playback");
+        
+       
+            if (pattern != null)
+            {
+                Debug.Log("pattern count: " + pattern.Count);
+                for (int p = 0; p < pattern.Count; p++)
+                {
+
+                
+                if (pattern[p] == 0)
+                    {
+                    Debug.Log("made it to red");
+                 //   int count = 0;
+
+                    //Red.GetComponent<Renderer>().material.color = Color.red;
+                    // Red.transform.Rotate(Vector3.up, Time.deltaTime * 30, Space.World);  
+                    /*
+                    while (count <500000)
+                    {
+                        Red.transform.Rotate(Vector3.up * Time.deltaTime);
+                        count++;
+                        */
+                        yield return new WaitForSeconds(0.6f);
+                        Red.GetComponent<Renderer>().material.color = Color.white;
+                        Red.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime *500000, Space.World);
+                        yield return new WaitForSeconds(0.6f);
+                        Red.transform.Rotate(new Vector3(0,180, 0), Time.deltaTime * 500000, Space.World);
+                        yield return new WaitForSeconds(0.6f);
+                        Red.transform.GetComponent<Renderer>().material.color = Color.red;
+                        yield return new WaitForSeconds(0.3f);
+
+                    }
+
+                    else if (pattern[p] == 2)
+                    {
+                 
+                        Debug.Log("made it to blue");
+                    
+                       
+                       
+
+                        yield return new WaitForSeconds(0.6f);
+                        Blue.GetComponent<Renderer>().material.color = Color.white;
+                    Blue.transform.Rotate(new Vector3(0,180,0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Blue.transform.Rotate(new Vector3(0,180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);     
+                    Blue.GetComponent<Renderer>().material.color = Color.blue;
+                    yield return new WaitForSeconds(0.3f);
+                       
+                }
+
+                
+            }
+            }
+        yield return new WaitForSeconds(0.7f);
+
+
+
+    }
+
+   
+}
---
+++ flashanimation2.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class flashanimation2 {
+
+    public IEnumerator playBack(List<int> pattern, GameObject Red, GameObject Blue, GameObject Green, GameObject Yellow)
+    {
+
+        if (pattern != null)
+        {
+            for (int p = 0; p < pattern.Count; p++)
+            {
+                if (pattern[p] == 0)
+                {
+                    yield return new WaitForSeconds(0.6f);
+                    Red.GetComponent<Renderer>().material.color = Color.white;
+                    Red.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Red.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Red.transform.GetComponent<Renderer>().material.color = Color.red;
+                    yield return new WaitForSeconds(0.3f);
+                }
+                else if (pattern[p] == 1)
+                {
+                    yield return new WaitForSeconds(0.6f);
+                    Green.GetComponent<Renderer>().material.color = Color.white;
+                    Green.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Green.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Green.transform.GetComponent<Renderer>().material.color = Color.green;
+                    yield return new WaitForSeconds(0.3f);
+                }
+                else if (pattern[p] == 2)
+                {
+                    yield return new WaitForSeconds(0.6f);
+                    Blue.GetComponent<Renderer>().material.color = Color.white;
+                    Blue.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Blue.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Blue.GetComponent<Renderer>().material.color = Color.blue;
+                    yield return new WaitForSeconds(0.3f);
+
+                }
+                else if (pattern[p] == 3)
+                {
+                    yield return new WaitForSeconds(0.6f);
+                    Yellow.GetComponent<Renderer>().material.color = Color.white;
+                    Yellow.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Yellow.transform.Rotate(new Vector3(0, 180, 0), Time.deltaTime * 500000, Space.World);
+                    yield return new WaitForSeconds(0.6f);
+                    Yellow.GetComponent<Renderer>().material.color = Color.yellow;
+                    yield return new WaitForSeconds(0.3f);
+
+
+                }
+
+
+            }
+          
+        }
+        yield return new WaitForSeconds(0.7f);
+    }
+}
